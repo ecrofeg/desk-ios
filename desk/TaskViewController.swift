4ebdab51d2b749f0f4c57ff95a84fc8aa3d0ebef
pnaumov <ecrofeg1@gmail.com>
2017-12-09 22:35:33 +0400

    Tab icon

--- desk/TaskViewController.swift
+++ desk/TaskViewController.swift
@@ -6,32 +6,46 @@ class TaskViewController: UIViewController {
     
     public var taskId: Int!
     private var task: Task?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationItem.title = "..."
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
         
-        Alamofire.request("http://localhost:5555/api/task/\(self.taskId!)").responseJSON { response in
-            switch response.result {
-            case .success(let value):
-                let tasks = JSON(value)["tasks"].dictionary!
-                
-                if let task = tasks[String(self.taskId)] {
-                    self.task = Task(
-                        id: task["id"].int!,
-                        title: task["name"].string!,
-                        description: task["description"].string!,
-                        priority: task["priority"].int!,
-                        createdAt: task["created_at"].string!,
-                        updatedAt: task["updated_at"].string!
-                    )
+        if let task = self.task {
+            loadPage(task: task)
+        }
+        else {
+            Alamofire.request("http://localhost:5555/api/task/\(self.taskId!)").responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let tasks = JSON(value)["tasks"].dictionary!
                     
-                    self.navigationItem.title = task["name"].string!
+                    if let taskJSON = tasks[String(self.taskId)] {
+                        let task = Task(
+                            id: taskJSON["id"].int!,
+                            title: taskJSON["name"].string!,
+                            description: taskJSON["description"].string!,
+                            priority: taskJSON["priority"].int!,
+                            createdAt: taskJSON["created_at"].string!,
+                            updatedAt: taskJSON["updated_at"].string!
+                        )
+                        
+                        self.task = task
+                        self.loadPage(task: task)
+                    }
+                    
+                case .failure(let error):
+                    print(error)
                 }
-                
-            case .failure(let error):
-                print(error)
             }
         }
+    }
+    
+    func loadPage(task: Task) {
+        navigationItem.title = task.title
     }
 }
