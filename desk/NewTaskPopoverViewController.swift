95f941b05b7693e00b806fdf38fa676e8c95bc99
pnaumov <ecrofeg1@gmail.com>
2017-12-17 01:27:04 +0400

    Proper font color for project name

--- desk/NewTaskPopoverViewController.swift
+++ desk/NewTaskPopoverViewController.swift
@@ -1,19 +1,24 @@
 import UIKit
 
 class NewTaskPopoverViewController: UIViewController, DelegateProject {
+    private var selectedProject: Project?
     @IBOutlet weak var projectToggler: UIView!
     @IBOutlet weak var selectedProjectLabel: UILabel!
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true)
     }
-    
-    private var selectedProject: Project?
+
+    func selectProject() {
+        selectedProject = nil
+        selectedProjectLabel.text = "None"
+        selectedProjectLabel.textColor = UIColor.lightGray
+    }
     
     func selectProject(project: Project) {
         selectedProject = project
         selectedProjectLabel.text = project.title
-        selectedProjectLabel.textColor = UIColor.black
+        selectedProjectLabel.textColor = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- desk/SelectProjectTableViewController.swift
+++ desk/SelectProjectTableViewController.swift
@@ -3,6 +3,7 @@ import Alamofire
 import SwiftyJSON
 
 protocol DelegateProject {
+    func selectProject()
     func selectProject(project: Project)
 }
 
@@ -10,18 +11,26 @@ class SelectProjectTableViewController: UITableViewController {
     var delegate: DelegateProject?
     private var projects = [Project]()
 
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        // Clear selected project
+        delegate?.selectProject()
+
+        // Go backwards
+        navigationController?.popViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         Alamofire.request("http://localhost:5555/api/project").responseJSON { response in
             switch response.result {
                 case .success(let value):
                     guard let tasks = JSON(value)["projects"].dictionary else {
                         return
                     }
-                    
+
                     var projectsArray = [Project]()
-                    
+
                     for (_, projectJSON) in tasks {
                         let project = Project(
                             id: projectJSON["id"].int!,
@@ -30,16 +39,16 @@ class SelectProjectTableViewController: UITableViewController {
                             createdAt: projectJSON["created_at"].string!,
                             updatedAt: projectJSON["updated_at"].string!
                         )
-                        
+
                         projectsArray.append(project)
                     }
-                    
+
                     // Store sorted projects
                     self.projects = projectsArray
-                    
+
                     // Re-render table's content
                     self.tableView.reloadData()
-                
+
                 case .failure(let error):
                     print(error)
             }
@@ -53,22 +62,22 @@ class SelectProjectTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return projects.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "selectProjectCell", for: indexPath)
         let project = projects[indexPath.row]
-        
+
         cell.textLabel?.text = project.title
-        
+
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let project = projects[indexPath.row]
-        
+
         // Pass selected project back to form
         delegate?.selectProject(project: project)
-        
+
         // Go backwards
         navigationController?.popViewController(animated: true)
     }
